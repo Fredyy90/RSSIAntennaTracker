6e6f17fa01569b5ff4bf4cf7085d4c85670079c5
Frederick Behrends <frederick.behrends@gmail.com>
2015-02-09 22:34:20 +0100

    added comments to config.h

--- Config.h
+++ Config.h
@@ -22,8 +22,8 @@
 #define SERVO_PAN_STEP 2
 
 #define SERVO_TILT_PIN 45
-#define SERVO_TILT_MIN 1850    // tilt servo horizontal in ms
-#define SERVO_TILT_MAX 975     // tilt servo vertical in ms
+#define SERVO_TILT_MIN 1850    // tilt servo bottom limit in ms
+#define SERVO_TILT_MAX 975     // tilt servo upper limit in ms
 #define SERVO_TILT_STEP 2
 
 #define SCAN_DELAY 50
@@ -64,9 +64,9 @@
 ##############################
 **/
 
-#define SERVO_MIN 0
-#define SERVO_MAX 256
-#define SERVO_MID SERVO_MAX/2
+#define SERVO_MIN 0            // internal servo position bottom/left
+#define SERVO_MAX 256          // internal servo position top/right
+#define SERVO_MID SERVO_MAX/2  // internal servo position center/center
 
 #define SERVO_DIRECTION_LEFT 1
 #define SERVO_DIRECTION_RIGHT 2
