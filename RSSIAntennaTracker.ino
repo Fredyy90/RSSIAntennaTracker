5b85cefa419ef7bbac929940a3b33de39e0611c6
Frederick Behrends <frederick.behrends@gmail.com>
2015-02-09 23:01:06 +0100

    added memory debugging via serial0

--- RSSIAntennaTracker.ino
+++ RSSIAntennaTracker.ino
@@ -6,6 +6,7 @@ int servoPositionTilt = SERVO_MID;
 void setup()
 {
 
+    Serial.begin(115200);
     lcdInit();
     servoInit();
 
@@ -16,7 +17,7 @@ void loop()
 
     triggerUpdates();
     servoDemo();
-
+    Serial.println(freeRam());
 }
 
 void triggerUpdates()
@@ -25,4 +26,11 @@ void triggerUpdates()
     rssiUpdate();
     lcdUpdate();
 
+}
+
+int freeRam ()
+{
+  extern int __heap_start, *__brkval;
+  int v;
+  return (int) &v - (__brkval == 0 ? (int) &__heap_start : (int) __brkval);
 }
